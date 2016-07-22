38e347a4bac6f22c424c269183a2d19ab0fd668b
HuXiLiFeng <1134575480@qq.com>
2016-07-22 20:36:47 +0800

    adding license
    adding license

---
+++ register.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2016/7/22 20:29
+# @Author  : Lee
+# @contact  : 1134575480@qq.com
+# @Site    : recommender system
+# @File    : register.py
+
+# set product key using GraphLab Create API
+import graphlab
+#graphlab.product_key.set_product_key('B7B9-6420-3F35-CAB2-A88B-79D7-60D8-6917')
+
+print graphlab.product_key.get_product_key()
